415e02d9f01da8570ed83b63d4e8e7b367441e7a
Abhinav pundir <abhinavpundir048@gmail.com>
2026-03-19 17:27:16 +0530

    Add binary tree height calculation and tree construction in DAY47.C

---
+++ DAY47.C
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *left, *right;
+};
+
+struct node* createNode(int data) {
+    struct node* newNode = (struct node*)malloc(sizeof(struct node));
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+struct node* buildTree(int arr[], int n, int index) {
+    if (index >= n || arr[index] == -1)
+        return NULL;
+
+    struct node* root = createNode(arr[index]);
+    root->left = buildTree(arr, n, 2 * index + 1);
+    root->right = buildTree(arr, n, 2 * index + 2);
+
+    return root;
+}
+
+int height(struct node* root) {
+    if (root == NULL)
+        return 0;
+
+    int leftHeight = height(root->left);
+    int rightHeight = height(root->right);
+
+    if (leftHeight > rightHeight)
+        return leftHeight + 1;
+    else
+        return rightHeight + 1;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    struct node* root = buildTree(arr, n, 0);
+
+    printf("%d", height(root));
+
+    return 0;
+}
