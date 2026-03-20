db48b8ec3a8a67d37faf4dc94658ccc19cc3cae5
Abhinav pundir <abhinavpundir048@gmail.com>
2026-03-20 17:05:59 +0530

    Add binary tree leaf node counting implementation in DAY48.C

---
+++ DAY48.C
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node* left;
+    struct node* right;
+};
+
+struct node* createNode(int data) {
+    struct node* newNode = (struct node*)malloc(sizeof(struct node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+int countLeafNodes(struct node* root) {
+    if (root == NULL)
+        return 0;
+
+    if (root->left == NULL && root->right == NULL)
+        return 1;
+
+    return countLeafNodes(root->left) + countLeafNodes(root->right);
+}
+
+int main() {
+    struct node* root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+    root->right->left = createNode(4);
+    root->right->right = createNode(5);
+
+    int leafCount = countLeafNodes(root);
+
+    printf("Number of leaf nodes = %d", leafCount);
+
+    return 0;
+}
