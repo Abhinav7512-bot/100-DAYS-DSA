6bfd62ef51b5b6556556417128a768fe95d53816
Abhinav pundir <abhinavpundir048@gmail.com>
2026-04-17 19:01:03 +0530

    Add implementation to find the maximum length of a subarray with zero sum

---
+++ DAY75.C
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int maxLen(int arr[], int n) {
+    int maxLength = 0, sum = 0;
+
+    for(int i = 0; i < n; i++) {
+        sum = 0;
+        for(int j = i; j < n; j++) {
+            sum += arr[j];
+            if(sum == 0) {
+                if(j - i + 1 > maxLength)
+                    maxLength = j - i + 1;
+            }
+        }
+    }
+
+    return maxLength;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("%d\n", maxLen(arr, n));
+
+    return 0;
+}
