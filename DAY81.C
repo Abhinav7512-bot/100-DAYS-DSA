86cfbbdf4f80374616bd5341eaa341c51ff50a06
Abhinav pundir <abhinavpundir048@gmail.com>
2026-04-28 13:17:20 +0530

    Refactor code to implement bubble sort algorithm and remove unused Floyd-Warshall logic

--- DAY81.C
+++ DAY81.C
@@ -1,45 +1,26 @@
 #include <stdio.h>
 
-#define INF 1000000000
-
 int main() {
     int n;
     scanf("%d", &n);
 
-    int dist[n][n];
+    int arr[n];
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            int x;
-            scanf("%d", &x);
-            if (x == -1 && i != j)
-                dist[i][j] = INF;
-            else
-                dist[i][j] = x;
-        }
-    }
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
 
-    for (int k = 0; k < n; k++) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (dist[i][k] < INF && dist[k][j] < INF) {
-                    if (dist[i][j] > dist[i][k] + dist[k][j]) {
-                        dist[i][j] = dist[i][k] + dist[k][j];
-                    }
-                }
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (dist[i][j] == INF)
-                printf("-1 ");
-            else
-                printf("%d ", dist[i][j]);
-        }
-        printf("\n");
-    }
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
 
     return 0;
 }
